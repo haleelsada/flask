dbc1b6c67e59658499c7797b7f068d49cd3a18b1
Haleelsada <75977159+haleelsada@users.noreply.github.com>
2023-10-28 19:39:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,54 @@
-from flask import Flask, request, jsonify
+import pickle 
+import spacy
+from flask import Flask,request
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+
+nlp = spacy.load("en_core_web_sm")
+
+happiness='happiness.pkl'
+anger='anger.pkl'
+love='love.pkl'  
+nuetral='nuetral.pkl'  
+saddness='saddness.pkl' 
+
+vect='vectorizer.pkl'
+vectorizer = TfidfVectorizer()
+
+
+with open(happiness, 'rb') as file:  
+    happiness = pickle.load(file)
+with open(anger, 'rb') as file:  
+    anger = pickle.load(file)
+with open(love, 'rb') as file:  
+    love = pickle.load(file)
+with open(nuetral, 'rb') as file:  
+    nuetral = pickle.load(file)
+with open(saddness, 'rb') as file:  
+    saddness = pickle.load(file)
+with open(vect, 'rb') as file:  
+    vectorizer = pickle.load(file)
+
+
+def emotion(text):
+	input_processed = ' '.join([token.lemma_ for token in nlp(text) if not token.is_stop])
+	input_vectorized = vectorizer.transform([input_processed])
+	return {'happiness':happiness.predict(input_vectorized)[0],'saddness':saddness.predict(input_vectorized)[0],'anger':anger.predict(input_vectorized)[0],'love':love.predict(input_vectorized)[0],'nuetral':nuetral.predict(input_vectorized)[0]}
+	
 app = Flask(__name__)
-#cors = CORS(app)
-#app.config['CORS_HEADERS'] = 'Content-Type'
-
-@app.route('/test',methods=['POST','GET'])
-def sentanalase():
-	print('api called')
-	
-	
-	return jsonify({'result':'result'})
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
- 
+def hello_world():
+    return 'Hello from Flask!'
 
+@app.route('/test',methods=['GET', 'POST'])
+def main():
+    #if request.method == 'POST':
+    #    text = request.form['input']
+    #    return emotion(text)
+    
+    return emotion('I am feeling low')
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    
+    app.run(debug=True)
